7f92c2533d8668dfdbfd00ca07022785a4ef3c4c
Nostrad4mus <n62707291@gmail.com>
2025-07-25 18:37:29 -0400

    Revisando Version de Gunicorn

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ pip install --user gunicorn
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python -m gunicorn --version  # Debe mostrar la versión
