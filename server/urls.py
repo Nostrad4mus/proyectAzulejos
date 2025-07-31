bc3dc0124529b0c7e7d4c00e42d7a83ca619be37
Nostrad4mus <n62707291@gmail.com>
2025-07-31 15:25:47 -0400

    Probando solucionar STATIC 4

--- server/urls.py
+++ server/urls.py
@@ -101,7 +101,7 @@ urlpatterns = [
     path('admin-dashboard/gestion-usuarios/<int:pk>/eliminar/', views.eliminar_usuario, name='eliminar_usuario'),
     
     
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
 
